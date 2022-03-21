bbeca6ce013672d2823ca1073ef8cca0aac4c1d9
Alexander Fedorov <hamster2240@gmail.com>
2022-03-21 17:31:49 +0200

    Array. Refactor Plus One.

--- Array/plus-one.js
+++ Array/plus-one.js
@@ -1,22 +1,16 @@
 // Задача на leetcode: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/559/
 
 var plusOne = function (digits) {
-	let carry = 0;
 	for (var i = digits.length - 1; i >= 0; i--) {
 		if (digits[i] === 9) {
 			if (i === 0) {
 				digits[i] = 0;
 				digits.unshift(1);
 			} else {
-				carry = 1;
 				digits[i] = 0;
 			}
 		} else {
-			if (carry === 1) {
-				digits[i] = digits[i] + carry;
-			} else {
-				digits[digits.length - 1]++;
-			}
+			digits[i]++;
 			break;
 		}
 	}
